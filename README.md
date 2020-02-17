cca7f749b159ffe78aa6bc25e6d416af8d9fead9
abond421 <alex.bond.3797@gmail.com>
2020-02-17 11:12:16 +1100

    Adding basic structure to build on.

---
+++ README.md
@@ -0,0 +1,1 @@
+Calculator Project as part of The Odin Project's Curriculum
---
+++ script.js
@@ -0,0 +1,35 @@
+//Add numbers
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log("Adding " + add(1, 1));
+
+//Subtract numbers
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+console.log("Subtracting " +subtract(1,-1));
+
+
+//Divide Numbers
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log("Dividing " + divide(1, 1));
+
+//Multiply Numbers
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log("Multiplying " + multiply(4, 4));
+
+//Raise to power
+function exponent(num1, num2) {
+    return num1 ** num2;
+}
+
+console.log ("Exponent " + exponent(3,3));
