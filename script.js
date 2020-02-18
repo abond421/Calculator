166083c1d01c6a26dc560714b505283240621905
abond421 <alex.bond.3797@gmail.com>
2020-02-18 12:05:32 +1100

    Calculator functions

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
-//Add numbers
-function add(num1, num2) {
-    return num1 + num2;
+const buttons = document.querySelectorAll(".btn");
+const formula = document.querySelector("#input");
+const deleteBtn = document.querySelectorAll(".delete");
+const result = document.querySelector("#result");
+
+for (const button of buttons) {
+  button.addEventListener("click", e => {
+    console.log("you clicked: ", button.innerHTML);
+
+    if (button.innerHTML == "AC") {
+      wipeFormula();
+    } else if (button.innerHTML == "DEL") {
+      deleteLast();
+    } else if (button.innerHTML == "=") {
+      result.innerHTML = calculate(formula.innerHTML);
+    } else if (button.innerHTML == "Ans") {
+      formula.innerHTML = result.innerHTML;
+    } else {
+      formula.innerHTML += button.innerHTML;
+    }
+  });
 }
 
-console.log("Adding " + add(1, 1));
+function calculate(formula) {
+  const noWsStr = formula.replace(/\s/g, "");
+  const operator = noWsStr.replace(/[\d.,]/g, "").split("");
+  const operand = noWsStr
+    .replace(/[+/%*-]/g, " ")
+    .replace(/\,/g, ".")
+    .split(" ")
+    .map(parseFloat)
+    .filter(it => it);
 
-//Subtract numbers
-function subtract(num1, num2) {
-    return num1 - num2;
+  if (operator.length >= operand.length) {
+    throw new Error("Too many operators!");
+  }
+
+  while (operator.includes("*")) {
+    let placement = operator.indexOf("*");
+    operand.splice(placement, 2, operand[placement] * operand[placement + 1]);
+    operator.splice(placement, 1);
+  }
+  while (operator.includes("/")) {
+    let placement = operator.indexOf("/");
+    operand.splice(placement, 2, operand[placement] / operand[placement + 1]);
+    operator.splice(placement, 1);
+  }
+  while (operator.includes("%")) {
+    let placement = operator.indexOf("%");
+    operand.splice(placement, 2, operand[placement] % operand[placement + 1]);
+    operator.splice(placement, 1);
+  }
+
+  let result = operand[0];
+  for (let i = 0; i < operator.length; i++) {
+    operator[i] === "+"
+      ? (result += operand[i + 1])
+      : (result -= operand[i + 1]);
+  }
+  return result;
 }
 
-console.log("Subtracting " +subtract(1,-1));
-
-
-//Divide Numbers
-function divide(num1, num2) {
-    return num1 / num2;
+function wipeFormula() {
+  formula.innerHTML = "";
+  result.innerHTML = "";
 }
 
-console.log("Dividing " + divide(1, 1));
-
-//Multiply Numbers
-function multiply(num1, num2) {
-    return num1 * num2;
+function deleteLast() {
+  formula.innerHTML = formula.innerHTML.slice(0, -1);
 }
-
-console.log("Multiplying " + multiply(4, 4));
-
-//Raise to power
-function exponent(num1, num2) {
-    return num1 ** num2;
-}
-
-console.log ("Exponent " + exponent(3,3));
